5c9b504dcdc9e3fa923132123d6bbdcf1062c566
OtonoKakuei <kakueioke@gmail.com>
2016-11-11 19:29:05 +0100

    Created a class for implementing the QSF.

---
+++ QueryRecWB/src/QuerySimilarityFunction.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import largespace.business.RowInfo;
+
+public class QuerySimilarityFunction {
+	
+	public Set<RowInfo> getSimilarQueries(RowInfo rowInfo) {
+		Set<RowInfo> similarQueries = new HashSet<>();
+		
+		return similarQueries;
+	}
+
+}
