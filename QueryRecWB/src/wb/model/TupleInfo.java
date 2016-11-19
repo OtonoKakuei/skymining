61460e308bacbbb3bd97319c7240dcec4427e4ef
OtonoKakuei <kakueioke@gmail.com>
2016-11-19 15:51:35 +0100

    Added equals and hash methods

--- QueryRecWB/src/wb/model/TupleInfo.java
+++ QueryRecWB/src/wb/model/TupleInfo.java
@@ -28,6 +28,43 @@ public class TupleInfo {
 	public Long getTableId() {
 		return tableId;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((keyId == null) ? 0 : keyId.hashCode());
+		result = prime * result + ((sequence == null) ? 0 : sequence.hashCode());
+		result = prime * result + ((tableId == null) ? 0 : tableId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TupleInfo other = (TupleInfo) obj;
+		if (keyId == null) {
+			if (other.keyId != null)
+				return false;
+		} else if (!keyId.equals(other.keyId))
+			return false;
+		if (sequence == null) {
+			if (other.sequence != null)
+				return false;
+		} else if (!sequence.equals(other.sequence))
+			return false;
+		if (tableId == null) {
+			if (other.tableId != null)
+				return false;
+		} else if (!tableId.equals(other.tableId))
+			return false;
+		return true;
+	}
 	
 
 }
