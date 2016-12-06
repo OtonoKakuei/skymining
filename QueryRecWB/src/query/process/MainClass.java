410ea09c911f2a203b3a88d0032a41d23b397d2c
OtonoKakuei <kakueioke@gmail.com>
2016-12-06 22:52:17 +0100

    Added a method to test/perform the QSF.

--- QueryRecWB/src/query/process/MainClass.java
+++ QueryRecWB/src/query/process/MainClass.java
@@ -1,14 +1,26 @@
 package query.process;
+import java.util.List;
+import java.util.Set;
+
 import com.beust.jcommander.JCommander;
 
+import largespace.business.DatabaseInteraction;
 import largespace.business.OptionsOwn;
 
 public class MainClass {
+	private static final MainClass INSTANCE = new MainClass();
+	
+	private MainClass() {
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 		OptionsOwn opt = new OptionsOwn();
 		QueryRec rec = new QueryRec(opt);
 		new JCommander(opt, args);
-
+		
+		INSTANCE.doQuerySimilarityFunction(opt);
+		
 		switch (opt.mode) {
 		// preprocessing step store the result of the query to our internal
 		// database
@@ -41,5 +53,53 @@ public class MainClass {
 			break;
 		}
 
+	}
+
+	private void doQuerySimilarityFunction(OptionsOwn opt) {
+		DatabaseInteraction.establishConnection(opt);
+		int i = 1;
+		int j = 1;
+		Long beginTime = System.currentTimeMillis();
+		List<Long> allComparableSequences = DatabaseInteraction.getAllComparableSequences();
+		allComparableSequences.removeAll(DatabaseInteraction.getAllProcessedSimilarSequences());
+//		allComparableSequences.removeAll(DatabaseInteraction.getAllSequencesFromQuerySimilarity());
+		int size = allComparableSequences.size();
+		double maxTime = 0.0;
+		double totalTime = 0.0;
+		double averageTime = 0.0;
+		for (Long seq : allComparableSequences) {
+			if (j % 500 == 0) {
+				DatabaseInteraction.establishConnection(opt.serverAddress, opt.username, opt.password);
+			}
+			Long beginLoopTime = System.currentTimeMillis();
+			System.out.println("Doing seq: " + seq + ", query: " + j + "/" + size);
+			Set<Long> similarSequences = QuerySimilarityFunction.getSimilarSequences(seq);
+			System.out.print("Obtained similar sequences! ");
+			//ONLY NEED TO SAVE THE SEQS ONCE! ONCE THEY'RE STORED IN THE DB, DO NOT STORE THEM AGAIN
+			//COMMENT FROM HERE
+			DatabaseInteraction.saveSimilarSequences(seq, similarSequences);
+			System.out.print("Saved similar sequences! ");
+			//COMMENT UNTIL HERE
+			double timeNeeded = 0.0;
+			if (similarSequences.size() < 100000) {
+				DatabaseInteraction.saveQuerySimilarity(seq, QuerySimilarityFunction.getSimilarQueries(seq, similarSequences));
+				timeNeeded = ((System.currentTimeMillis() - beginLoopTime)) / 1000.0;
+				System.err.println("Query number: " + i + ", Took: " + timeNeeded + " seconds.");
+				i++;
+			} else {
+				timeNeeded = ((System.currentTimeMillis() - beginLoopTime)) / 1000.0;
+				System.err.println("Missed, took: " + timeNeeded + " seconds.");
+			}
+			totalTime += timeNeeded;
+			averageTime = totalTime / j;
+			if (maxTime < timeNeeded) {
+				maxTime = timeNeeded;
+			}
+			
+			System.err.println("TotalTime: " + totalTime + ", AverageTime: " + averageTime + ", MaxTime: " + maxTime);
+			
+			j++;
+		}
+		System.out.println("Finished in: " + ((System.currentTimeMillis() - beginTime)) / 1000 + " seconds.");
 	}
 }
