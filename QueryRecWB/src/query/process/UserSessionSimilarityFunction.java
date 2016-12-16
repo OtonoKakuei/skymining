02e7a5fe9cbc208093873d1aceacfd878c96e8b7
Tsven <t.hristoforov@abv.bg>
2016-12-16 16:47:08 +0100

    added UserSessionSimilarityFunction

---
+++ QueryRecWB/src/query/process/UserSessionSimilarityFunction.java
@@ -0,0 +1,16 @@
+package query.process;
+
+import java.util.Map;
+
+public class UserSessionSimilarityFunction {
+
+	private static final UserSessionSimilarityFunction INSTANCE = new UserSessionSimilarityFunction();
+
+	private UserSessionSimilarityFunction() {
+
+	}
+
+	public static Map<Long, Long> GetSimilarQueries(int userSession, long seq) {
+		return null;
+	}
+}
